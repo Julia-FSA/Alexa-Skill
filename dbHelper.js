57040d1f152cd1614b81739ff6c153b1012b128f
johnoh26 <john.oh26@gmail.com>
2020-05-13 14:52:27 -0400

    find by ingredient works

--- dbHelper.js
+++ dbHelper.js
@@ -1,86 +1,112 @@
-var AWS = require("aws-sdk");
+var AWS = require('aws-sdk')
 // const { v4: uuidv4 } = require("uuid");
+const getFromSpoon = require('./spoonacular')
 
-AWS.config.update({region: "us-east-2"});
-const tableName = "ingredients";
+AWS.config.update({region: 'us-east-2'})
+const tableName = 'ingredients'
 
-var dbHelper = function () { };
-var docClient = new AWS.DynamoDB.DocumentClient();
+var dbHelper = function () {}
+var docClient = new AWS.DynamoDB.DocumentClient()
 
 dbHelper.prototype.addIngredientToFridge = (ingredient, unit) => {
-    return new Promise(async (resolve, reject) => {
-        let result = await docClient.get({
-            TableName: 'stocks',
-            Key: {id: 0}
-          }).promise()
-        let prevIngredients = result.Item.ingredients;
-        const params = {
-            TableName: 'stocks',
-            Key:{id:0},
-            UpdateExpression: 'set ingredients = :ingred',
-            ExpressionAttributeValues: {
-              ':ingred': [...prevIngredients, 
-                {"ingredientName": `${ingredient}`,
-                "ingredientQuantity": 1,
-                "unit":`${unit}`
-              }]
-            }
-        };
-        docClient.update(params, (err, data) => {
-            if (err) {
-                console.log("Unable to update =>", JSON.stringify(err))
-                return reject("Unable to update");
-            }
-            console.log("Saved Data, ", JSON.stringify(data));
-            resolve(data);
-        });
-    });
+  return new Promise(async (resolve, reject) => {
+    let result = await docClient
+      .get({
+        TableName: 'stocks',
+        Key: {id: 0},
+      })
+      .promise()
+    let prevIngredients = result.Item.ingredients
+    const params = {
+      TableName: 'stocks',
+      Key: {id: 0},
+      UpdateExpression: 'set ingredients = :ingred',
+      ExpressionAttributeValues: {
+        ':ingred': [
+          ...prevIngredients,
+          {
+            ingredientName: `${ingredient}`,
+            ingredientQuantity: 1,
+            unit: `${unit}`,
+          },
+        ],
+      },
+    }
+    docClient.update(params, (err, data) => {
+      if (err) {
+        console.log('Unable to update =>', JSON.stringify(err))
+        return reject('Unable to update')
+      }
+      console.log('Saved Data, ', JSON.stringify(data))
+      resolve(data)
+    })
+  })
 }
 
-dbHelper.prototype.getRecipe = (userID) => {
-    return new Promise((resolve, reject) => {
-        const params = {
-            TableName: tableName,
-            KeyConditionExpression: "#userID = :user_id",
-            ExpressionAttributeNames: {
-                "#userID": "userId"
-            },
-            ExpressionAttributeValues: {
-                ":user_id": userID
-            }
-        }
-        docClient.query(params, (err, data) => {
-            if (err) {
-                console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-                return reject(JSON.stringify(err, null, 2))
-            } 
-            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
-            resolve(data.Items)
-            
+dbHelper.prototype.getRecipe = async (alexaId) => {
+  return new Promise(async (resolve, reject) => {
+    // step 1. get userId tied to alexaId
+    try {
+      let data = await docClient
+        .scan({
+          TableName: 'users',
+          FilterExpression: '#alexa_id = :alexa_id',
+          ExpressionAttributeNames: {
+            '#alexa_id': 'alexa_id',
+          },
+          ExpressionAttributeValues: {
+            ':alexa_id': alexaId,
+          },
         })
-    });
+        .promise()
+      let {stock_id} = data.Items[0]
+      console.log('getUser succeeded! StockId is:', stock_id)
+
+      // step 2. get ingredients in stock
+      var params = {
+        TableName: 'stocks',
+        Key: {
+          id: stock_id,
+        },
+      }
+      data = await docClient.get(params).promise()
+      let ingredientsArray = Object.keys(data.Item.ingredients)
+      console.log('getStock succeeded! You have:', ingredientsArray)
+
+      // step 3. ping Spoonacular API
+      getFromSpoon('findByIngredients', 0, ingredientsArray, null)
+    } catch (err) {
+      console.error(
+        'Unable to read stock. Error JSON:',
+        JSON.stringify(err, null, 2)
+      )
+    }
+  })
 }
 
 dbHelper.prototype.removeRecipe = (movie, userID) => {
-    return new Promise((resolve, reject) => {
-        const params = {
-            TableName: tableName,
-            Key: {
-                "userId": userID,
-                "movieTitle": movie
-            },
-            ConditionExpression: "attribute_exists(movieTitle)"
-        }
-        docClient.delete(params, function (err, data) {
-            if (err) {
-                console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
-                return reject(JSON.stringify(err, null, 2))
-            }
-            console.log(JSON.stringify(err));
-            console.log("DeleteItem succeeded:", JSON.stringify(data, null, 2));
-            resolve()
-        })
-    });
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: tableName,
+      Key: {
+        userId: userID,
+        movieTitle: movie,
+      },
+      ConditionExpression: 'attribute_exists(movieTitle)',
+    }
+    docClient.delete(params, function (err, data) {
+      if (err) {
+        console.error(
+          'Unable to delete item. Error JSON:',
+          JSON.stringify(err, null, 2)
+        )
+        return reject(JSON.stringify(err, null, 2))
+      }
+      console.log(JSON.stringify(err))
+      console.log('DeleteItem succeeded:', JSON.stringify(data, null, 2))
+      resolve()
+    })
+  })
 }
 
-module.exports = new dbHelper();
+module.exports = new dbHelper()
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
 // Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
 // session persistence, api calls, and more.
-const Alexa = require('ask-sdk-core');
+const Alexa = require('ask-sdk-core')
 const dbHelper = require('./dbHelper')
 const generalPrompt = 'Is there anything else I can do?'
-let fridge = [];
-let ingredient = [];
+let fridge = []
+let ingredient = []
+let recipe = []
 // const axios = require('axios')
 // const id = 9226
 // const spoonacularAPIKey = 'ba51c1e2686d4c01abb5c4cdf16fb881'
@@ -15,21 +16,23 @@ let ingredient = [];
 //         console.log('ingredient is', res.data.name)
 //     })
 
-
 //ADD A COMMENT
 
 const LaunchRequestHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Welcome to Julia Cooks. How can I help you today. I am using amazon lambda from Digital Wednesday';
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
-};
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest'
+    )
+  },
+  handle(handlerInput) {
+    const speakOutput =
+      'Welcome to Julia Cooks. How can I help you today. This is John Oh Version'
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse()
+  },
+}
 // const HelloWorldIntentHandler = {
 //     canHandle(handlerInput) {
 //         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
@@ -44,194 +47,243 @@ const LaunchRequestHandler = {
 //     }
 // };
 const getPriceOfFoodHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'getPriceOfFood';
-    },
-    handle(handlerInput) {
-        const request = handlerInput.requestEnvelope.request
-        let speakOutput = '';
-        let slotValues = getSlotValues(request.intent.slots);
-        if(slotValues && slotValues.food){
-            speakOutput = `The price of ${slotValues.food.heardAs} is 2 dollars`;
-        } else {
-            speakOutput = 'Hello are you asking for food prices';
-        }
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(generalPrompt)
-            .getResponse();
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === 'getPriceOfFood'
+    )
+  },
+  handle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request
+    let speakOutput = ''
+    let slotValues = getSlotValues(request.intent.slots)
+    if (slotValues && slotValues.food) {
+      speakOutput = `The price of ${slotValues.food.heardAs} is 2 dollars`
+    } else {
+      speakOutput = 'Hello are you asking for food prices'
     }
-};
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(generalPrompt)
+      .getResponse()
+  },
+}
 const addToFridgeHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'addToFridge';
-    },
-    handle(handlerInput) {
-        const request = handlerInput.requestEnvelope.request
-        let speakOutput = '';
-        let slotValues = getSlotValues(request.intent.slots);
-        if(slotValues && slotValues.food){
-            speakOutput = `Added ${slotValues.food.heardAs} to the fridge`;
-            fridge.push(slotValues.food.heardAs);
-            dbHelper.addIngredientToFridge(slotValues.food.heardAs, 'each')
-        } else {
-            speakOutput = 'Hello are you asking for food prices';
-        }
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(generalPrompt)
-            .getResponse();
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === 'addToFridge'
+    )
+  },
+  handle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request
+    let speakOutput = ''
+    let slotValues = getSlotValues(request.intent.slots)
+    if (slotValues && slotValues.food) {
+      speakOutput = `Added ${slotValues.food.heardAs} to the fridge`
+      fridge.push(slotValues.food.heardAs)
+      dbHelper.addIngredientToFridge(slotValues.food.heardAs, 'each')
+    } else {
+      speakOutput = 'Hello are you asking for food prices'
     }
-};
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(generalPrompt)
+      .getResponse()
+  },
+}
 const getFridgeHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'getFridge';
-    },
-    handle(handlerInput) {
-        const speakOutput = `There is ${fridge.join(", ")} in your fridge`;
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
-};
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === 'getFridge'
+    )
+  },
+  handle(handlerInput) {
+    const speakOutput = `There is ${fridge.join(', ')} in your fridge`
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(
+        'add a reprompt if you want to keep the session open for the user to respond'
+      )
+      .getResponse()
+  },
+}
 const getIngredientHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'getIngredient';
-    },
-    handle(handlerInput) {
-        const speakOutput = `Your ingredient is ${ingredient[0]}.`;
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
-};
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === 'getIngredient'
+    )
+  },
+  handle(handlerInput) {
+    const speakOutput = `Your ingredient is ${ingredient[0]}.`
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(
+        'add a reprompt if you want to keep the session open for the user to respond'
+      )
+      .getResponse()
+  },
+}
+// *******************************
+const findRecipeHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === 'findRecipe'
+    )
+  },
+  handle(handlerInput) {
+    const speakOutput = `We found a recipe.`
+    dbHelper.getRecipe('saltthis123')
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(
+        'add a reprompt if you want to keep the session open for the user to respond'
+      )
+      .getResponse()
+  },
+}
+// *******************************
+
 const HelpIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'You can say hello to me! How can I help?';
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
-};
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent'
+    )
+  },
+  handle(handlerInput) {
+    const speakOutput = 'You can say hello to me! How can I help?'
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse()
+  },
+}
 const CancelAndStopIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
-                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Goodbye!';
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .getResponse();
-    }
-};
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+      (Alexa.getIntentName(handlerInput.requestEnvelope) ===
+        'AMAZON.CancelIntent' ||
+        Alexa.getIntentName(handlerInput.requestEnvelope) ===
+          'AMAZON.StopIntent')
+    )
+  },
+  handle(handlerInput) {
+    const speakOutput = 'Goodbye!'
+    return handlerInput.responseBuilder.speak(speakOutput).getResponse()
+  },
+}
 const SessionEndedRequestHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
-    },
-    handle(handlerInput) {
-        // Any cleanup logic goes here.
-        return handlerInput.responseBuilder.getResponse();
-    }
-};
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) ===
+      'SessionEndedRequest'
+    )
+  },
+  handle(handlerInput) {
+    // Any cleanup logic goes here.
+    return handlerInput.responseBuilder.getResponse()
+  },
+}
 // The intent reflector is used for interaction model testing and debugging.
 // It will simply repeat the intent the user said. You can create custom handlers
 // for your intents by defining them above, then also adding them to the request
 // handler chain below.
 const IntentReflectorHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
-    },
-    handle(handlerInput) {
-        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
-        const speakOutput = `You just triggered ${intentName}`;
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
-};
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+    )
+  },
+  handle(handlerInput) {
+    const intentName = Alexa.getIntentName(handlerInput.requestEnvelope)
+    const speakOutput = `You just triggered ${intentName}`
+    return (
+      handlerInput.responseBuilder
+        .speak(speakOutput)
+        //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+        .getResponse()
+    )
+  },
+}
 // Generic error handling to capture any syntax or routing errors. If you receive an error
 // stating the request handler chain is not found, you have not implemented a handler for
 // the intent being invoked or included it in the skill builder below.
 const ErrorHandler = {
-    canHandle() {
-        return true;
-    },
-    handle(handlerInput, error) {
-        console.log(`~~~~ Error handled: ${error.stack}`);
-        const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
-};
+  canHandle() {
+    return true
+  },
+  handle(handlerInput, error) {
+    console.log(`~~~~ Error handled: ${error.stack}`)
+    const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse()
+  },
+}
 function getSlotValues(filledSlots) {
-    const slotValues = {};
-    Object.keys(filledSlots).forEach((item) => {
-        const name  = filledSlots[item].name;
-        if (filledSlots[item] &&
-            filledSlots[item].resolutions &&
-            filledSlots[item].resolutions.resolutionsPerAuthority[0] &&
-            filledSlots[item].resolutions.resolutionsPerAuthority[0].status &&
-            filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code) {
-            switch (filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code) {
-                case 'ER_SUCCESS_MATCH':
-                    slotValues[name] = {
-                        heardAs: filledSlots[item].value,
-                        resolved: filledSlots[item].resolutions.resolutionsPerAuthority[0].values[0].value.name,
-                        ERstatus: 'ER_SUCCESS_MATCH'
-                    };
-                    break;
-                case 'ER_SUCCESS_NO_MATCH':
-                    slotValues[name] = {
-                        heardAs: filledSlots[item].value,
-                        resolved: '',
-                        ERstatus: 'ER_SUCCESS_NO_MATCH'
-                    };
-                    break;
-                default:
-                    break;
-            }
-        } else {
-            slotValues[name] = {
-                heardAs: filledSlots[item].value,
-                resolved: '',
-                ERstatus: ''
-            };
-        }
-    }, this);
-    return slotValues;
+  const slotValues = {}
+  Object.keys(filledSlots).forEach((item) => {
+    const name = filledSlots[item].name
+    if (
+      filledSlots[item] &&
+      filledSlots[item].resolutions &&
+      filledSlots[item].resolutions.resolutionsPerAuthority[0] &&
+      filledSlots[item].resolutions.resolutionsPerAuthority[0].status &&
+      filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code
+    ) {
+      switch (
+        filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code
+      ) {
+        case 'ER_SUCCESS_MATCH':
+          slotValues[name] = {
+            heardAs: filledSlots[item].value,
+            resolved:
+              filledSlots[item].resolutions.resolutionsPerAuthority[0].values[0]
+                .value.name,
+            ERstatus: 'ER_SUCCESS_MATCH',
+          }
+          break
+        case 'ER_SUCCESS_NO_MATCH':
+          slotValues[name] = {
+            heardAs: filledSlots[item].value,
+            resolved: '',
+            ERstatus: 'ER_SUCCESS_NO_MATCH',
+          }
+          break
+        default:
+          break
+      }
+    } else {
+      slotValues[name] = {
+        heardAs: filledSlots[item].value,
+        resolved: '',
+        ERstatus: '',
+      }
+    }
+  }, this)
+  return slotValues
 }
 // The SkillBuilder acts as the entry point for your skill, routing all request and response
 // payloads to the handlers above. Make sure any new handlers or interceptors you've
 // defined are included below. The order matters - they're processed top to bottom.
 exports.handler = Alexa.SkillBuilders.custom()
-    .addRequestHandlers(
-        LaunchRequestHandler,
-        getPriceOfFoodHandler,
-        addToFridgeHandler,
-        getFridgeHandler,
-        getIngredientHandler,
-        HelpIntentHandler,
-        CancelAndStopIntentHandler,
-        SessionEndedRequestHandler,
-        IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
-    )
-    .addErrorHandlers(
-        ErrorHandler,
-    )
-    .lambda();
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    getPriceOfFoodHandler,
+    addToFridgeHandler,
+    getFridgeHandler,
+    getIngredientHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler,
+    findRecipeHandler,
+    IntentReflectorHandler // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda()
